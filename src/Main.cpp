b369875df4becb8e6b8628cabb73486e3de7904b
Sameer Mahant <smahant@SamLab.localdomain>
2019-04-09 20:51:13 +0530

    Added initial CMakeLists.txt and Main.cpp files

---
+++ src/Main.cpp
@@ -0,0 +1,8 @@
+#include <cstdlib>
+#include <iostream>
+
+auto main([[maybe_unused]] int argc, [[maybe_unused]] char** argv, [[maybe_unused]] char* envp[])
+    -> int
+{
+    return EXIT_SUCCESS;
+}
