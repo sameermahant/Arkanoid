aacd62322d3bd3d7dda4d39ac3ecc349bf4dacd2
Sameer Mahant <mahant.sameer@gmail.com>
2019-05-02 01:19:48 +0530

    Created basic window and game loop with SFML

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,12 @@
+#include "Game.hpp"
+
 #include <cstdlib>
 #include <iostream>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv, [[maybe_unused]] char* envp[])
-{
+int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv, [[maybe_unused]] char *envp[]) {
+
+    Game game;
+    game.runGameLoop();
+
     return EXIT_SUCCESS;
 }
